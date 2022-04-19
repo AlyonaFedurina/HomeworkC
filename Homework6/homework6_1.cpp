f5b259788bc13cfb84391d085cab65af72125c2a
AlyonaFedurina <fedurina.a@phystech.edu>
2022-04-19 19:29:54 +0300

    Add homework 6

---
+++ Homework6/homework6_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <istream>
+#include <locale>
+#include <ostream>
+#include <iostream>
+
+
+int main()
+{
+    std::fstream fin{ "strings.txt" };
+
+    std::string strData;
+    int N;
+
+    std::cin >> N;
+
+    fin.seekg((N-1)*10);             // переход к 5-му символу
+    // Получаем оставшуюся часть строки и распечатываем ее
+    getline(fin, strData);
+    std::cout << strData << '\n';
+}
+
